65a9c6b2b5334ab8a1964fc3621fc692dbcdd542
DavidKDevui <101657192+DavidKDevui@users.noreply.github.com>
2024-03-31 01:18:38 +0100

    Refonte

--- lookaround/src/Controller/IndexController.php
+++ lookaround/src/Controller/IndexController.php
@@ -32,50 +32,51 @@ class IndexController extends AbstractController
 
         return $this->render('index.html.twig', [
             'demandeForm' => $form->createView(),
-            "lightBackgroundNav" => false
+            "lightBackgroundNav" => false, 
+            "page" => "index"
         ]);
     }
     
     #[Route('/notre-histoire', name: 'notreHistoire')]
     public function notreHistoire(): Response
     {
-        return $this->render('notreHistoire.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('notreHistoire.html.twig', ["lightBackgroundNav" => true, "page" => "notreHistoire"]);
     }
 
     #[Route('/nos-réalisations', name: 'nosRealisations')]
     public function nosRealisations(DatabaseService $databaseService): Response
     {
-        return $this->render('nosRealisations.html.twig', ["visites" => $databaseService->getListeVisites(), "lightBackgroundNav" => true]);
+        return $this->render('nosRealisations.html.twig', ["visites" => $databaseService->getListeVisites(), "lightBackgroundNav" => true, "page" => "nosRealisations"]);
     }
 
     #[Route('/nos-tarifs', name: 'nosTarifs')]
     public function nosTarifs(): Response
     {
-        return $this->render('nosTarifs.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('nosTarifs.html.twig', ["lightBackgroundNav" => true, "page" => "nosTarifs"]);
     }
 
     #[Route('/trouver-une-visite', name: 'trouverUneVisite')]
     public function trouverUneVisite(): Response
     {
-        return $this->render('trouverUneVisite.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('trouverUneVisite.html.twig', ["lightBackgroundNav" => true, "page" => "trouverUneVisite"]);
     }
 
     #[Route('/politique-confidentialité', name: 'politiqueConfidentialite')]
     public function politiqueConfidentialite(): Response
     {
-        return $this->render('politiqueConfidentialite.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('politiqueConfidentialite.html.twig', ["lightBackgroundNav" => true, "page" => "other"]);
     }
 
     #[Route('/conditions-generales', name: 'conditionsGenerales')]
     public function conditionsGenerales(): Response
     {
-        return $this->render('conditionsGenerales.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('conditionsGenerales.html.twig', ["lightBackgroundNav" => true, "page" => "other"]);
     }
 
     #[Route('/politique-cookies', name: 'politiqueCookies')]
     public function politiqueCookies(): Response
     {
-        return $this->render('politiqueCookies.html.twig', ["lightBackgroundNav" => true]);
+        return $this->render('politiqueCookies.html.twig', ["lightBackgroundNav" => true, "page" => "other"]);
     }
 
     #[Route('/scrollToForm', name: 'scrollToForm')]
